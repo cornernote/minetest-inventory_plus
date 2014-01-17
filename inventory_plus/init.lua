9503bd7d4a0fd9237ec4e5f646f173c4dbad0245
Brett O'Donnell <cornernote@gmail.com>
2014-01-17 10:57:41 +1030

    Update init.lua

--- inventory_plus/init.lua
+++ inventory_plus/init.lua
@@ -3,8 +3,8 @@
 Inventory Plus for Minetest
 
 Copyright (c) 2012 cornernote, Brett O'Donnell <cornernote@gmail.com>
-Source Code: https://github.com/cornernote/minetest-particles
-License: GPLv3
+Source Code: https://github.com/cornernote/minetest-inventory_plus
+License: BSD-3-Clause https://raw.github.com/cornernote/minetest-inventory_plus/master/LICENSE
 
 ]]--
 
